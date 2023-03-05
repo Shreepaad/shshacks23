34ca2a1674a0ea0349c606bd1589499ae61cc295
Purpified <huskymaster12345hm@gmail.com>
2023-03-04 20:21:56 -0600

    sad

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,7 +3,7 @@ import React from "react";
 const About = () => {
     return (
         <div>
-            <h1>Athletics</h1>
+            <h1>Athleticals</h1>
             <p>Content</p>
         </div>
     );
