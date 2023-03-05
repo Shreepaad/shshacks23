1aed6388ea4794f1668eb4d481fec825802b4f4b
JRC-DXRK <jayeshroychoudhury.2007@gmail.com>
2023-03-05 03:00:24 -0600

    worked on xc template, tell me how it is

--- src/pages/cross_country.js
+++ src/pages/cross_country.js
@@ -6,14 +6,16 @@ const cross = () => {
        <div className='cross_country'>
         <h1>Cross Country</h1>
         <p><br/>A form of long-distance running that<br/> takes place on natural terrain, such as <br/>trails, hills, and grassy fields.</p>
-        <div className='card'>
-            
-
-            <div className='box1'>
-                <h2>Tryouts</h2>
-                <div className='box3'></div>
-                </div>
-            <div className='box2'></div>
+        
+        <div className='card1'>
+            <h2>Tryouts</h2>
+            <div className='c1box1'>
+                <p>hey bois<br/>hows it going</p>
+            </div>
+            <div className='c1box2'>
+                <img src="https://resources.finalsite.net/images/f_auto,q_auto/v1559589797/d125org/o48oo2dglaabdyqqyvrk/Scott_Sommers.png" className="coach"></img>
+                <h2>Scott Sommers</h2>
+            </div>
         </div>
        </div>
     );
