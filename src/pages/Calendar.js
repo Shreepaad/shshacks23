02974cff722d28c7046aab79da42ee64033139f5
alonr1807 <alonr1807@gmail.com>
2023-03-05 00:08:39 -0600

    II DID IT!!!! I DID IT I DI TItI ADOPDASKPDSD

--- src/pages/Calendar.js
+++ src/pages/Calendar.js
@@ -1,10 +1,45 @@
 import React from "react";
+import { initializeApp  } from 'https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js';
+import { getAuth, onAuthStateChanged, GoogleAuthProvider, signInWithPopup, signOut  } from 'https://www.gstatic.com/firebasejs/9.0.0/firebase-auth.js';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCEKA9Dql4I2PABOPBCTKrJE8POVUXpgmg",
+  authDomain: "sportif-973dd.firebaseapp.com",
+  projectId: "sportif-973dd",
+  storageBucket: "sportif-973dd.appspot.com",
+  messagingSenderId: "695901937762",
+  appId: "1:695901937762:web:b651907d68d404b366f13b",
+  measurementId: "G-0W3FF527SK"
+};
+initializeApp(firebaseConfig);
+const firebaseapp = initializeApp(firebaseConfig);
+const auth = getAuth(firebaseapp);
+const goog = new GoogleAuthProvider(auth);
+
+
+const signInWithGoogle = () => {
+    console.log("dsaljfsd");
+    signInWithPopup(auth, goog)
+    .then(() => {
+        window.location.assign('./profile');
+    })
+    .catch(error => {
+        console.error(error);
+    })
+}
+
+
 
 const Calendar = () => {
     return (
         <div>
             <h1>Calendar</h1>
             <p>Content</p>
+            <button onClick = {signInWithGoogle}> signinwithgooogle </button>
         </div>
     );
 };
