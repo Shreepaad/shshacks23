9566e6588e096d8ea4cdab8108ae9b255395312a
Shreepaad <85477483+Shreepaad@users.noreply.github.com>
2023-03-05 02:08:59 -0600

    Add files via upload

---
+++ src/pages/cross_country.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './sections.css';
+
+const cross = () => {
+    return (
+       <div className='cross_country'>
+        <h1>Cross Country</h1>
+        <p><br/>A form of long-distance running that<br/> takes place on natural terrain, such as <br/>trails, hills, and grassy fields.</p>
+        <div className='card'>
+            
+
+            <div className='box1'>
+                <h2>Tryouts</h2>
+                <div className='box3'></div>
+                </div>
+            <div className='box2'></div>
+        </div>
+       </div>
+    );
+}
+ 
+export default cross;
