6f26144779b93174981cabc8ddf9604a35d386d2
Shreepaad <85477483+Shreepaad@users.noreply.github.com>
2023-03-05 02:10:34 -0600

    Update Athletics.js

--- src/pages/Athletics.js
+++ src/pages/Athletics.js
@@ -1,10 +1,14 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Athletics = () => {
     return (
         <div className="section athletics">
             <h1>Athletics</h1>
             <p>Content</p>
+        NavLink to="/cross_country">
+                        <p>Cross_Country</p>
+            </NavLink>
             <div className="sportsSelector">
                 <div className="sportBox"></div>
                 <div className="sportBox2"></div>
