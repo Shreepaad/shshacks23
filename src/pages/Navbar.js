c62b6b9fbc1e94a87a1a77e909f2ff2c018cf397
Purpified <huskymaster12345hm@gmail.com>
2023-03-04 20:47:26 -0600

    test

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -9,13 +9,13 @@ import { NavLink } from "react-router-dom";
 const Navbar = () => {
     return (
         <div className="Sidebar">
-            <h1 className="logo">
-                <NavLink to="/">Sportif</NavLink>
-            </h1>
+            <h1 className="logo">Sportif</h1>
             <ul className="nav-list">
                 <li className="nav-item">
                     <NavLink to="/">
-                        <CalendarMonthOutlined />
+                        <CalendarMonthOutlined
+                            style={{ justifySelf: "flex-start" }}
+                        />
                         <p>Calendar</p>
                     </NavLink>
                 </li>
@@ -31,6 +31,14 @@ const Navbar = () => {
                         <p>Athletics</p>
                     </NavLink>
                 </li>
+
+                <li className="login">
+                    
+                    <SportsBasketballOutlined />
+                    <p>Name</p>
+                    
+                </li>
+                
             </ul>
         </div>
     );
